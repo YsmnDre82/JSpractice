5c5cdd5802c3ba6ab4dff273bbb321213b2a9f41
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-07-26 16:13:57 +0330

    Added switch case example with grouped conditions

--- main.js
+++ main.js
@@ -1,35 +1,15 @@
-var number1 = Number(prompt("enter number 1 :"));
+var browser = prompt("please enter your computer browser:");
 
-var sumString = "+";
-var minString = "-";
-var mulString = "*";
-var divString = "/";
-
-var Opertor = prompt(`please type operator
-    ${sumString}
-    ${minString}
-    ${mulString}
-    ${divString}
-    `);
-
-var number2 = Number(prompt("enter number 2 :"));
-
-function showResult(answer) {
-    return(`your answer is ${answer}`)
-};
-
-if (Opertor === sumString) {
-    alert(showResult(number1 + number2))
-}else if(Opertor === minString){
-    alert(showResult(number1 - number2))
-}else if(Opertor === mulString){
-    alert(showResult(number1 * number2))
-}else if(Opertor === divString){
-    if (number2 =! 0) {
-        alert(showResult(number1 / number2))
-    }else{
-        alert("error")
-    }
-}else{
-    alert("your type is wrong!")
+switch (browser) {
+    case "edge":
+        alert("you've got the Edge!");
+        break;
+    case "Chrom":    // if (browser == "Chrome" // browser == "FireFox"){}
+    case "FireFox":
+    case "Safari":
+    case "Opera":
+        alert("Okay we support these browsers too");
+    default:
+        alert("we hope tjat this page looks OK!")
+        break;
 }

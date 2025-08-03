7dccb4c27e5813fffef624208fb63cbb86685d82
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-08-03 20:13:33 +0330

    Build word gussing game using While loop

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-const magicNumber = 7;
+// const magicNumber = 7;
 
 // Math.random 
 // Math.floor
 // const magicNumber = Math.floor(Math.random() * 10) + 1;
 
 
-for (attempt = 1; attempt <= 5; attempt++) {
-  let guess = Number(prompt("عدد جادویی بین 1 تا 10 رو حدس بزن:"));
+// for (attempt = 1; attempt <= 5; attempt++) {
+//   let guess = Number(prompt("عدد جادویی بین 1 تا 10 رو حدس بزن:"));
 
-  console.log(guess === 0);
+//   console.log(guess === 0);
 
 
-  if (guess === 0) {
-    alert("شما منصرف شدید");
-    break
-  }
+//   if (guess === 0) {
+//     alert("شما منصرف شدید");
+//     break
+//   }
 
-  if (guess < 0) {
-    alert("عدد منفی مجاز نیست!");
-    continue
-  }
+//   if (guess < 0) {
+//     alert("عدد منفی مجاز نیست!");
+//     continue
+//   }
 
 
   //   Expression (عبارت)
@@ -44,6 +44,39 @@ for (attempt = 1; attempt <= 5; attempt++) {
 
 
   // guess === magicNumber ? (alert("آفرین! درست حدس زدی 🎉"),  break) : alert("اشتباهه! دوباره امتحان کن...");
+//   if (guess === magicNumber) {
+//     alert("آفرین! درست حدس زدی 🎉");
+//     break;
+//   } else {
+//     alert("اشتباهه! دوباره امتحان کن...");
+//   }
+
+//   // &&
+//   attempt === 5 && alert("تموم شد! عدد جادویی " + magicNumber + " بود.");
+// }
+
+
+
+
+
+const magicNumber = Math.ceil( Math.random()*10);
+
+let attempt = 1
+while ( attempt <= 5) {
+  attempt++
+  let guess = Number(prompt("عدد جادویی بین 1 تا 10 رو حدس بزن:"));
+
+  if (guess === 0) {
+    alert("شما منصرف شدید");
+    break
+  }
+  
+
+  if (guess < 0) {
+    alert("عدد منفی مجاز نیست!");
+    continue
+  }
+
   if (guess === magicNumber) {
     alert("آفرین! درست حدس زدی 🎉");
     break;
@@ -51,6 +84,5 @@ for (attempt = 1; attempt <= 5; attempt++) {
     alert("اشتباهه! دوباره امتحان کن...");
   }
 
-  // &&
   attempt === 5 && alert("تموم شد! عدد جادویی " + magicNumber + " بود.");
 }

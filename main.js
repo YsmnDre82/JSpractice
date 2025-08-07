a0de524745b0fe0b3351021de22db3888f5946c4
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-08-07 11:05:31 +0330

    form project part 2

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ let people = [];
 
 form.addEventListener("submit", function (e) {
   e.preventDefault()
-  console.log();
-  
+
   const newperson = {
     name: nameInput.value.trim(),
     family: familyInput.value.trim(),
@@ -23,6 +22,51 @@ form.addEventListener("submit", function (e) {
     gender: genderSelect.value
   }
 
-  console.log(newperson); 
-  
+  if (!newperson.name || !newperson.family || !newperson.email) {
+    alert("لطفاً فیلدهای الزامی را وارد کنید.")
+    return
+  }
+
+  people.push(newperson);
+  form.reset();
+
 })
+
+document.getElementById("showModal").addEventListener("click", () => {
+  if (people.length === 0) {
+    modal.innerHTML = `<h3>هنوز کاربری ثبت نشده است.</h3> `
+  } else {
+    modal.innerHTML = `<h3>لیست کاربران:</h3>`;
+    const list = document.createElement("ul");
+
+    people.map((person, index) => {
+
+      const { name,family,email,job,phone,gender } = person
+
+      const li = document.createElement("li");
+      li.innerText = `${index + 1} . ${name} ${family}
+       ایمیل: ${email} 
+       شغل: ${job || '---'} 
+       تلفن: ${phone || '---'} 
+       جنسیت: ${gender || '---'}`;
+      list.appendChild(li)
+    })
+
+    modal.appendChild(list);
+  }
+
+  overlay.style.opacity = "1";
+  overlay.style.visibility = "visible";
+  modal.style.opacity = "1";
+  modal.style.visibility = "visible";
+
+})
+
+
+// بستن مدال با کلیک روی پس‌زمینه
+overlay.addEventListener("click", () => {
+  overlay.style.opacity = "0";
+  overlay.style.visibility = "hidden";
+  modal.style.opacity = "0";
+  modal.style.visibility = "hidden";
+});

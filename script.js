74026171553b130e2b582a4f0f2c9d5e03a85a6b
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-08-19 10:24:28 +0330

    second commit from new project part 2

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ function loadUsers() {
       document.getElementById("table").style.opacity = "1";
       tbody.innerHTML = "";
       data.forEach(user => {
-        console.log(user);
-        tbody.innerHTML += `<tr>
+        tbody.innerHTML += `<tr id="user-${user.id}"> 
               <td>${user.id}</td>
               <td>${user.name}</td>
               <td>${user.phone}</td>
               <td>${user.email}</td>
               <td>${user.address.city}, ${user.address.street}</td>
-              <td><button  onclick="deleteUser(${user.id})">حذف</button></td>
+              <td>
+              <button  onclick="deleteUser(${user.id})">حذف</button>
+              <button onclick="editUser(${user.id}, '${user.name}', '${user.email}')">ویرایش</button>
+              </td>
             </tr>`;
       });
     });
@@ -37,7 +39,7 @@ function addUser() {
       console.log("POST response:", user);
 
       tbody.innerHTML += `<tr>
-        <td>  </td>
+        <td>${user.id}</td>
         <td>${user.name}</td>
         <td>  </td>
         <td>${user.email}</td>
@@ -59,4 +61,37 @@ function deleteUser(id) {
       alert(`کاربر با شناسه ${id} حذف شد (فقط در ظاهر، چون API واقعی نیست)`);
       loadUsers(); // بازخوانی لیست
     });
+}
+
+
+// ویرایش کاربر 
+function editUser(id, oldName, oldEmail) {
+  const newName = prompt("نام جدید را وارد کنید:", oldName);
+  const newEmail = prompt("ایمیل جدید را وارد کنید:", oldEmail);
+
+  if (!newName || !newEmail) {
+    alert("ورودی نامعتبر بود");
+    return;
+  }
+
+  fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({ name: newName, email: newEmail }),//name , email
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  })
+    .then(res => res.json())
+    .then(updated => {
+      console.log(updated);
+
+      alert("ویرایش انجام شد (نمایشی)");
+
+
+
+      // اعمال تغییرات به صورت نمایشی
+      const row = document.getElementById(`user-${id}`);
+      row.children[1].textContent = updated.name;
+      row.children[3].textContent = updated.email;
+
+    });
+
 }

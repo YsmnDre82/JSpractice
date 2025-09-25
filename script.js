857c678e5e7806ea4590edeca30f408cd668703e
YsmnDre82 <yasamandaraei1382@gmail.com>
2025-09-25 16:07:50 +0330

    practice Objects part 1

--- script.js
+++ script.js
@@ -1,44 +1,87 @@
-const addButtons = document.querySelectorAll(".add-to-cart");
-const cartItems = document.querySelector(".cart-items");
-const totalPriceElement = document.getElementById("total-price");
+// const addButtons = document.querySelectorAll(".add-to-cart");
+// const cartItems = document.querySelector(".cart-items");
+// const totalPriceElement = document.getElementById("total-price");
 
-let total = 0;
-let cart = []; // آرایه برای ذخیره محصولات
+// let total = 0;
+// let cart = []; // آرایه برای ذخیره محصولات
 
-addButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const product = button.parentElement;
-    const name = product.querySelector("h3").textContent;
-    const price = parseInt(product.querySelector(".price").dataset.price);
+// addButtons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     const product = button.parentElement;
+//     const name = product.querySelector("h3").textContent;
+//     const price = parseInt(product.querySelector(".price").dataset.price);
 
-    // چک کن آیا محصول از قبل تو سبد هست یا نه
-    const existingItem = cart.find(item => item.name === name);
-    if (existingItem) {
-      existingItem.quantity += 1;
-    } else {
-      cart.push({ name, price, quantity: 1 });
-    }
+//     // چک کن آیا محصول از قبل تو سبد هست یا نه
+//     const existingItem = cart.find(item => item.name === name);
+//     if (existingItem) {
+//       existingItem.quantity += 1;
+//     } else {
+//       cart.push({ name, price, quantity: 1 });
+//     }
 
-    // آپدیت نمایش سبد خرید
-    renderCart();
-  });
-});
+//     // آپدیت نمایش سبد خرید
+//     renderCart();
+//   });
+// });
 
-function renderCart() {
-  cartItems.innerHTML = ""; // پاک‌کردن لیست قبلی
-  total = 0;
+// function renderCart() {
+//   cartItems.innerHTML = ""; // پاک‌کردن لیست قبلی
+//   total = 0;
 
-  cart.forEach(item => {
-    const li = document.createElement("li");
-    li.textContent = `${item.name} × ${item.quantity}`;
-    const itemTotal = item.price * item.quantity;
-    const priceSpan = document.createElement("span");
-    priceSpan.textContent = itemTotal.toLocaleString() + " تومان";
-    li.appendChild(priceSpan);
-    cartItems.appendChild(li);
+//   cart.forEach(item => {
+//     const li = document.createElement("li");
+//     li.textContent = `${item.name} × ${item.quantity}`;
+//     const itemTotal = item.price * item.quantity;
+//     const priceSpan = document.createElement("span");
+//     priceSpan.textContent = itemTotal.toLocaleString() + " تومان";
+//     li.appendChild(priceSpan);
+//     cartItems.appendChild(li);
 
-    total += itemTotal;
-  });
+//     total += itemTotal;
+//   });
 
-  totalPriceElement.textContent = total.toLocaleString();
+//   totalPriceElement.textContent = total.toLocaleString();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let user = {
+    name : "yasi",
+    age : 22
 }
+
+// user.city = "hamedan";
+// user["job"] = "frontend developer";
+
+// user.age = 24;
+
+// delete user.city;
+
+console.log(user);
